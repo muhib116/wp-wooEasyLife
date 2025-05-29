692a94045051d0425e6a326962fc4add6c55bed9
Muhibbullah Ansary <40915304+muhib116@users.noreply.github.com>
2025-05-29 23:24:11 +0600

    Fix Preview Setup

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
-import NewRequisitionForm from './components/NewRequisitionForm.vue'
 
-const app = createApp(App)
-
-// Register the NewRequisitionForm as a global component
-app.component('NewRequisitionForm', NewRequisitionForm)
-
-app.mount('#app')
+createApp(App).mount('#app')
